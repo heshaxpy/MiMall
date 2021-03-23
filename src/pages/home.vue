b9c2abfb50fdc2aab96522899c2b1497f1eb855b
heshaxpy <2322152681@qq.com>
2021-03-23 21:30:08 +0800

    新增

---
+++ src/pages/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <nav-header></nav-header>
+    <router-view></router-view>
+    <nav-header></nav-header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "nav-footer",
+};
+</script>
